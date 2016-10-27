5e0e717bdb0260e3cbfbdf2528198b28322d82a8
Travis Mitchell <mitch809@reliant.morris.umn.edu>
2016-10-27 16:18:39 -0500

    Added a comment telling us where to add the search into the experiment.

--- src/simple_search/experiment.clj
+++ src/simple_search/experiment.clj
@@ -37,10 +37,6 @@
              (:run-number result)
              (long (:score @(:answer result))))))
 
-;; This really shouldn't be necessary, as I should have included the labels
-;; in the maps when generated the problem files. Unfortunately I only just
-;; realized that, and it's easier to do this than have everyone merge in a
-;; new set of problem files to all your projects.
 (defn get-labelled-problem
   "Takes the name of a problem (as a string) and returns the actual
    problem instance (as a map) with the name added to the map under
@@ -74,10 +70,12 @@
                       {:label "hill_climber_penalized_score"})
                     (with-meta (partial core/random-search core/score)
                       {:label "random_search"})]
+                  ;;Insert search algorithm to the above bracket.
                    (map get-labelled-problem
-                        ["knapPI_11_20_1000_4" "knapPI_13_20_1000_4" "knapPI_16_20_1000_4"
-                         "knapPI_11_200_1000_4" "knapPI_13_200_1000_4" "knapPI_16_200_1000_4"])
+                        ["knapPI_16_200_1000_4"]
+                        ;;add things to the above bracket to run on multiple problem-sets.
+                        )
                    (Integer/parseInt num-repetitions)
                    (Integer/parseInt max-answers)))
   (shutdown-agents))
-
+(-main "1" "1")
